713173d217348ea4c5f70aae4128794d90875f7e
Cullen King <cullen@ridewithgps.com>
2014-03-28 16:36:12 -0700

    Ele lookup bugs, can now change data paths.

--- lib/humps.rb
+++ lib/humps.rb
@@ -2,17 +2,37 @@ require 'rexml/document'
 require 'open-uri'
 
 class Humps
+  #SRTM_PATH = "#{Rails.root}/gisdata/srtm/"
+  #NED_PATH = "#{Rails.root}/gisdata/ned/"
+  #ASTER_PATH = "#{Rails.root}/gisdata/aster/"
   SRTM_PATH = "/var/gisdata/srtm/"
   NED_PATH = "/var/gisdata/ned/ned_13_arc_second_1_degree_tiles/"
-  GI_PATH = "/var/gisdata/gi/"
   ASTER_PATH = "/var/gisdata/aster/"
+
+  def self.srtm_path
+    @srtm_path || SRTM_PATH
+  end
+  def self.srtm_path= path
+    @srtm_path = path
+  end
+  def self.ned_path
+    @ned_path || NED_PATH
+  end
+  def self.ned_path= path
+    @ned_path = path
+  end
+  def self.aster_path
+    @aster_path || ASTER_PATH
+  end
+  def self.aster_path= path
+    @aster_path = path
+  end
 
 
   #fall through various sources in order of importance
   def self.get_ele(x,y)
     return -9999 unless x && y
-    if y > 59.9 #this is ghetto, but might be GI related, so high latitude
-      #ele = self.bilinear_gi(x, y)
+    if y >= 60
       ele = self.bilinear_aster(x, y)# if ele.nil? or ele < -420
     else
       ele = self.bilinear_ned(x,y)
@@ -45,7 +65,7 @@ class Humps
   def self.nn_aster(x,y)
     hdr = self.get_hdr(:aster, x, y)
     return -9999 if hdr.nil?
-    return nn(hdr, x, y, 2, 'ss')
+    return nn(hdr, x, y, 2, 's')
   end
 
   def self.bilinear_aster(x, y)
@@ -58,26 +78,6 @@ class Humps
     hdr = get_hdr(:aster, x,y)
     return -9999 if hdr.nil?
     return cubic(hdr, x, y, 2, 'ssss')
-  end
-
-
-  ####### GI related calls ################
-  def self.nn_gi(x,y)
-    hdr = self.get_hdr(:gi, x, y)
-    return -9999 if hdr.nil?
-    return nn(hdr, x, y, 4, 'f')
-  end
-
-  def self.bilinear_gi(x, y)
-    hdr = self.get_hdr(:gi, x, y)
-    return -9999 if hdr.nil?
-    return bilinear(hdr, x, y, 4, 'ff')
-  end
-
-  def self.cubic_gi(x, y)
-    hdr = get_hdr(:gi, x,y)
-    return -9999 if hdr.nil?
-    return cubic(hdr, x, y, 4, 'ffff')
   end
 
 
@@ -105,7 +105,7 @@ class Humps
   def self.nn_ned(x,y)
     hdr = get_hdr(:ned, x,y)
     return -9999 if hdr.nil?
-    return nn(hdr, x, y, 4, 'ff')
+    return nn(hdr, x, y, 4, 'f')
   end
 
   def self.bilinear_ned(x, y)
@@ -124,11 +124,13 @@ class Humps
   ########## base methods ############33333
   def self.nn(hdr, x, y, bytes, pack)
     begin
+      #puts hdr
       x_offset = ((x-hdr[:min_x]) / hdr[:cellsize]).round
       y_offset = (((hdr[:min_y] + hdr[:cellsize]*(hdr[:ncols]-1)) - y) / hdr[:cellsize]).round
       byte_offset = bytes*(x_offset + hdr[:ncols]*y_offset)
+      #puts "#{x_offset},#{y_offset}"# - ele: #{ele}"
       ele = IO.read(hdr[:data_file], bytes, byte_offset).unpack(pack)[0]
-      return (10 * ele).round / 10.0
+      return ele.round(1)
     rescue
       return -9999
     end
@@ -153,7 +155,7 @@ class Humps
       a3, a4 = IO.read(hdr[:data_file], num_bytes_to_read, byte_offset2).unpack(pack)
     rescue
       #we wrapped around our DEM edges, let's just get nearest neighbor for simplicity in this rare case
-      return self.nn_gi(x, y)
+      return self.nn_srtm(x, y)
     end
 
     #if all values are nodata, then it's not a local fluke/void, let's return nodata value
@@ -165,11 +167,11 @@ class Humps
     a4 = 0 if a4 < -420
 
     res = a1 + (a2 - a1)*x_index + (a3 - a1)*y_index + (a1 - a2 - a3 + a4)*x_index*y_index
-    (10 * res).round / 10.0
+    res.round(1)
   end
 
   def self.cubic(hdr, x, y, bytes, pack)
-    ty = hdr[:min_y] + hdr[:cellsize] * (hdr[:nrows] - 1)
+    ty = hdr[:min_y] + hdr[:cellsize] * (hdr[:ncols]-1)
     x_offset_abs = (x - hdr[:min_x]) / hdr[:cellsize]
     y_offset_abs = (ty - y) / hdr[:cellsize]
     x_offset = x_offset_abs.floor
@@ -207,7 +209,7 @@ class Humps
     #puts row2.inspect
     #puts row1.inspect
     #puts row0.inspect
-    (10 * self.cubic_interpolate([i0, i1, i2, i3], y)).round / 10.0
+    self.cubic_interpolate([i0, i1, i2, i3], y).round(1)
   end
 
 
@@ -264,45 +266,6 @@ class Humps
     puts "saved smoothed gain/loss: #{asset.elevation_gain}, #{asset.elevation_loss}"
   end
 
-  def self.usgs_ele(x,y)
-    begin
-      xml = open("http://gisdata.usgs.gov/XMLWebServices/TNM_Elevation_Service.asmx/getElevation?X_Value=#{x}&Y_Value=#{y}&Elevation_Units=METERS&Source_Layer=&Elevation_Only=true").read
-    rescue Timeout::Error
-      return false
-    end
-
-    doc = REXML::Document.new(xml)
-    #ele = (10.0 * doc.elements['double'].text.to_f).round / 10.0
-    ele = doc.elements['double'].text.to_f
-    if ele < -1000
-      #usgs retrns a very large negative, -1.79e308 or so when can't find a value, and
-      #since the lowest place on earth is -422 meters (dead sea), checking < -1000 works
-      return -9999
-    end
-    return (10.0 * ele).round / 10.0 #geto round!
-  end
-
-  def self.geonames_ele(x, y)
-    return self.geonames_eles([{'x' => x, 'y' => y}])[0]
-  end
-
-  def self.geonames_eles(points)
-    eles = []
-    points.each_slice(20) do |pts|
-      lats = []
-      lngs = []
-      pts.each { |p| lats << p['y']; lngs << p['x'] }
-
-      res = open("http://ws.geonames.org/srtm3?lats=#{lats.join(',')}&lngs=#{lngs.join(',')}")
-      eles += res.read.split("\n").map do |e|
-        parsed = e.strip.to_i
-        parsed < -400 ? -9999 : parsed
-      end
-      sleep(1)
-    end
-    return eles
-  end
-
   def self.get_grid(ll_x, ll_y, ur_x, ur_y, num_x, num_y)
     x_spacing = (ur_x - ll_x) / num_x
     y_spacing = (ur_y - ll_y) / num_y
@@ -348,32 +311,24 @@ class Humps
       #ok, so it took me a while to figure this out, but these aren't too magic
       #of numbers...each tile doesn't start on a clean number.  The min-y of
       #a tile may be 46.999861.  So we have to offset our number by the remainder.
-      x_offset = (x-0.000139).floor.abs
-      y_offset = (y-0.000139).floor.abs
-      fn_base = "#{ASTER_PATH}ASTGTM_#{n}%02d#{e}%03d_dem" % [y_offset, x_offset]
-    when :gi
-      #each file is a 1x1 degree chunk, named n[lat.floor]e[lng.floor.flt
-      x_offset = (x+0.00005).floor.abs
-      y_offset = (y+0.000075).floor.abs
-      fn_base = "#{GI_PATH}n#{y_offset}e0%02d" % x_offset
-      return fn_base + '.hdr', fn_base + '.flt'
+      x_offset = x.floor.abs
+      y_offset = y.floor.abs
+      fn_base = "#{aster_path}ASTGTM_#{n}%02d#{e}%03d_dem" % [y_offset, x_offset]
     when :srtm
       #these aren't that magical, we have a 5 degree tile system so we have to
       #do a little math to make sure we chunk by degrees of five.  The magic looking
       #offset numbers are because each tile does not start/end on a clean decimal
       #boundary, so we have to offset by the amount of overlap between degrees.
-      filex = ((x + 179.999584) / 5.0).ceil
-      filey = 24 - ((y + 60.0004168) / 5.0).floor
-      fn_base = "#{SRTM_PATH}srtm_%.2d_%.2d" % [filex, filey]
+      #filex = ((x + 179.999584) / 5.0).ceil
+      #filey = 24 - ((y + 60.0004168) / 5.0).floor
+      filex = ((x + 180 + (5/6000.0)) / 5.0).ceil
+      filey = (24 - ((y + 60 + (5/6000.0)) / 5.0)).ceil
+      fn_base = "#{srtm_path}srtm_%.2d_%.2d" % [filex, filey]
     when :ned
       return nil if x > 0 || y < 0 #we only have a certain range in the NED
-      x_offset = (x+0.000555555).abs.ceil
-      y_offset = (y+0.000555555).abs.ceil
-      fn_base = NED_PATH + "dem#{x_offset}#{y_offset}"
-      #key_x = (x.to_i - 1).abs.to_s #file name doesn't have a negative
-      #key_y = (y.to_i + 1).to_s
-      #the file format is dem[lng.floor.abs][lat.floor].hdr
-      #fn_base = NED_PATH + "dem#{key_x + key_y}"
+      x_offset = x.abs.ceil
+      y_offset = y.abs.ceil
+      fn_base = ned_path + "dem#{x_offset}#{y_offset}"
     else
       return nil
     end

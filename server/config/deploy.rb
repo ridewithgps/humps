6873758a81c0730e834f345d7cff1495579a0041
Cullen King <cullen@ridewithgps.com>
2015-06-25 10:05:54 -0700

    elevation server pointing at kona and bailey now

--- server/config/deploy.rb
+++ server/config/deploy.rb
@@ -12,7 +12,7 @@ ssh_options[:forward_agent] = true
 
 set (:deploy_to) { "/var/www/#{application}" }
 
-role :app, "bruno", "fiona"
+role :app, "kona", "bailey"
 set :application, 'humps'
 set :user, 'deploy'
 set :use_sudo, false
@@ -29,6 +29,13 @@ end
 
 after :deploy do
   deploy.cleanup
+end
+
+after "deploy:setup" do
+  #ensure proper directory structure is created
+  %w{/log /tmp /tmp/pids /vendor /vendor/bundle}.each do |file|
+    run "mkdir -p #{shared_path}#{file}"
+  end
 end
 
 after "deploy:update_code" do
@@ -66,7 +73,21 @@ namespace :deploy do
   desc "'nuff said"
   task :default do
     update
+    bundle_install
     restart
+  end
+
+  task :bundle_install, :roles => [:app] do
+    # using some ideas from https://github.com/carlhuda/bundler/blob/master/lib/bundler/deployment.rb
+    bundle_flags = "--deployment --quiet"
+    bundle_dir = File.join(fetch(:shared_path), 'vendor', 'bundle')
+    args = ["--path #{bundle_dir}"]
+    args << "--without development test"
+    args << bundle_flags
+    # before we use '--without development test' need to make sure
+    # jenkins can install test gems. once we use the without i think
+    # we can remove the RAILS_ENV check in Gemfile
+    run "cd #{release_path} && bundle install #{args.join(' ')}"
   end
 
   desc "Restart application"
